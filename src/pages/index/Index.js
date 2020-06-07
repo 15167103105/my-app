e199e7ba0425921f68e9b905115c8f32dce57163
贺婷婷 <yuexi.htt@raycloud.com>
2020-06-07 22:59:43 +0800

    feat: hook学习

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // useState就是一个Hook,在函数组件里调用它来给组件添加一些内部state，React会在重复渲染时保留这个state。
 // useState会返回一对值：当前状态和一个让你更新它的函数，你可以在事件处理函数中或其他一些地方调用这个函数。
@@ -6,6 +6,12 @@ import React, { useState } from 'react';
 export default function Example() {
     const [count, setCount] = useState(0);
     console.log('useState', useState(0));
+
+    // useEffect 是一个Effect Hook，给函数组件增加了操作副作用的能力，它跟class组件中的componentDidMount、componentDidUpdate、
+    // componentWillUnmount具有相同的用途，只不过被合成了一个API
+    useEffect(() => {
+        document.title = `你点击了${count}次`;
+    });
     return(
         <div>
             <p>你点击了{count}次</p>
